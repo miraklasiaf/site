38c8de83096873d98509e28b3feffcf240c8c2ed
Faisal Karim <faisalkarim96@gmail.com>
2020-09-17 14:41:13 +0700

    fix: dashboard views

--- src/pages/api/page-views.js
+++ src/pages/api/page-views.js
@@ -3,8 +3,10 @@ import db from '@/lib/db-admin'
 export default (req, res) => {
   if (!req.query.id) {
     return db.ref('views').once('value', (snapshot) => {
-      const views = snapshot.val()
-      const allViews = Object.values(views).reduce((total, value) => total + value)
+      const { blog } = snapshot.val()
+      const allViews = Object.values(blog).reduce((total, value) => total + value)
+
+      console.log(allViews)
 
       return res.status(200).json({
         total: allViews
