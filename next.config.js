aa5b7e831db7b3c7986652b87e428a64544a334a
Faisal Karim <faisalkarim96@gmail.com>
2020-12-03 18:37:55 +0700

    fix: edit url

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withPlugins = require('next-compose-plugins')
 const withMdxEnhanced = require('next-mdx-enhanced')
 const { getEditUrl, addLeadingSlash } = require('@docusaurus/utils')
 
-const EDIT_URL = 'https://github.com/miraklasiaf/miraklasiaf.com/edit/develop/src/pages'
+const EDIT_URL = 'https://github.com/miraklasiaf/miraklasiaf.com/blob/develop/pages'
 
 function fileToPath(str) {
   return addLeadingSlash(str.replace('.mdx', ''))
