29fccfd7c677e830cf4a211256110640f2a804cd
Faisal Karim <faisalkarim96@gmail.com>
2021-01-30 10:27:43 +0700

    feat: add rss

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   webpack: (config, { isServer }) => {
     if (isServer) {
       require('./scripts/generate-sitemap')
+      require('./scripts/generate-rss')
     }
 
     return config
