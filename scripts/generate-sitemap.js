e5cb220f2a7c972f6a59f9ccc7b6c265a18dc692
Faisal Karim <faisalkarim96@gmail.com>
2021-07-20 16:20:15 +0700

    Fix sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,21 +1,33 @@
 const fs = require('fs')
+
 const globby = require('globby')
 const prettier = require('prettier')
 
 ;(async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
-  const pages = await globby(['pages/**/*{.js,.mdx}', '!pages/_*.js', '!pages/api'])
+  const pages = await globby([
+    'pages/*.js',
+    'data/**/*.mdx',
+    '!data/*.mdx',
+    '!pages/_*.js',
+    '!pages/api',
+    '!pages/404.js'
+  ])
+
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             ${pages
               .map((page) => {
-                const path = page.replace('pages', '').replace('.js', '').replace('.mdx', '')
+                const path = page
+                  .replace('pages', '')
+                  .replace('data', '')
+                  .replace('.js', '')
+                  .replace('.mdx', '')
                 const route = path === '/index' ? '' : path
-
                 return `
                         <url>
-                            <loc>${`https://miraklasiaf.now.sh${route}`}</loc>
+                            <loc>${`https://miraklasiaf.com${route}`}</loc>
                         </url>
                     `
               })
