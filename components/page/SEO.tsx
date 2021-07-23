8e2e029ec62b6aac499f7d3fbf7952f62fdf0550
Faisal Karim <faisalkarim96@gmail.com>
2021-07-23 22:24:52 +0700

    Fix seo for blog pages

--- components/page/SEO.tsx
+++ components/page/SEO.tsx
@@ -1,34 +1,59 @@
-import { NextSeo } from 'next-seo'
+import { NextSeo, ArticleJsonLd } from 'next-seo'
 import { siteMetadata } from '@/config'
 import { useRouter } from 'next/router'
 
 interface SEOProps {
   title: string
-  description: string
-  canonical?: string
+  description?: string
+  author?: string
+  date?: string
 }
 
-export default function SEO({ title, description }: SEOProps) {
+export default function SEO({ title, description, author, date }: SEOProps) {
   const router = useRouter()
+  const url = `${siteMetadata.seo.openGraph.url}${router.asPath}`
+  const defaultOgUrl = `${siteMetadata.seo.openGraph.url}/static/images/og.jpg`
+  const generateOgUrl = `${siteMetadata.seo.openGraph.url}/api/og?title=${title}`
 
   return (
-    <NextSeo
-      title={title}
-      description={description}
-      canonical={`${siteMetadata.seo.openGraph.url}${router.asPath}`}
-      openGraph={{
-        url: `${siteMetadata.seo.openGraph.url}${router.asPath}`,
-        title: title,
-        description: description,
-        images: [
-          {
-            url: `${siteMetadata.seo.openGraph.url}/static/images/og.jpg`,
-            alt: title,
-            width: 1280,
-            height: 720
-          }
-        ]
-      }}
-    />
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={url}
+        openGraph={{
+          // blog page seo
+          type: date && 'article',
+          article: date && {
+            publishedTime: date
+          },
+          url: url,
+          title: title,
+          description: description,
+          images: [
+            {
+              url: date ? generateOgUrl : defaultOgUrl,
+              alt: title,
+              width: 1280,
+              height: 720
+            }
+          ]
+        }}
+      />
+      // blog page seo
+      {date && (
+        <ArticleJsonLd
+          url={url}
+          title={title}
+          description={description}
+          images={[generateOgUrl]}
+          datePublished={date}
+          dateModified={date}
+          authorName={author}
+          publisherLogo="/static/favicons/android-chrome-192x192.png"
+          publisherName={author}
+        />
+      )}
+    </>
   )
 }
