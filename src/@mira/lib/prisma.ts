627a7c7b5338a798d0477506f5beecfd3e99a037
Faisal Karim <faisalkarim96@gmail.com>
2022-01-09 14:28:15 +0700

    Use best practice

--- src/@mira/lib/prisma.ts
+++ src/@mira/lib/prisma.ts
@@ -1,21 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
-
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  console.log(process.env.NODE_ENV);
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export default prisma;
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query']
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
--- src/pages/api/views/[slug].ts
+++ src/pages/api/views/[slug].ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@mira/lib/prisma';
+import { prisma } from '@mira/lib/prisma';
 
 export default async function handler(
   req: NextApiRequest,
