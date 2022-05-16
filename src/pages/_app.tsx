6872632d57569a0151dc2da37fc8d48c5126088d
Faisal Karim <faisalkarim96@gmail.com>
2022-05-16 09:50:57 +0700

    remove ga and update uses page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import { ThemeProvider } from 'next-themes';
-import { useGa } from '@mira/hooks';
 import '../styles/global.css';
 
 const App = ({ Component, pageProps }) => {
-  useGa();
-
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,60 +1,54 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import { GAScript } from '@mira/lib/ga';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends NextDocument {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" />
-          <meta
-            name="msapplication-config"
-            content="/static/favicon/browserconfig.xml"
-          />
-          <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
-          <link rel="manifest" href="/static/favicon/site.webmanifest" />
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="msapplication-config"
+          content="/static/favicon/browserconfig.xml"
+        />
+        <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
+        <link rel="manifest" href="/static/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/static/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link
+          rel="preload"
+          href="/static/fonts/inter-var-latin.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/static/favicon/apple-touch-icon.png"
+          sizes="180x180"
+        />
+        {[16, 32].map((size) => (
           <link
-            rel="mask-icon"
-            href="/static/favicon/safari-pinned-tab.svg"
-            color="#5bbad5"
+            key={size}
+            rel="icon"
+            type="image/png"
+            sizes={`${size}x${size}`}
+            href={`/static/favicon/favicon-${size}x${size}.png`}
           />
-          <link
-            rel="preload"
-            href="/static/fonts/inter-var-latin.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="apple-touch-icon"
-            href="/static/favicon/apple-touch-icon.png"
-            sizes="180x180"
-          />
-          {[16, 32].map((size) => (
-            <link
-              key={size}
-              rel="icon"
-              type="image/png"
-              sizes={`${size}x${size}`}
-              href={`/static/favicon/favicon-${size}x${size}.png`}
-            />
-          ))}
-          <meta
-            name="google-site-verification"
-            content={process.env.GOOGLE_SITE_VERIFICATION}
-          />
-          <meta name="monetization" content={process.env.ILP_URL} />
-        </Head>
-        <body className="bg-white dark:bg-black text-white dark:text-black">
-          <GAScript />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+        ))}
+        <meta
+          name="google-site-verification"
+          content={process.env.GOOGLE_SITE_VERIFICATION}
+        />
+        <meta name="monetization" content={process.env.ILP_URL} />
+      </Head>
+      <body className="bg-white dark:bg-black text-white dark:text-black">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
